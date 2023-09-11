41afd9458b2ab9d14f0779506b831801ed4a8071
Arsalan Ahmad Quraishi <54109777+arsalan-zeus@users.noreply.github.com>
2023-09-11 05:44:39 +0530

    add stock cam

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@ $(call inherit-product, device/realme/sm8250-common/common.mk)
 # Inherit proprietary libraries
 $(call inherit-product, vendor/realme/bitra/bitra-vendor.mk)
 
+# Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
